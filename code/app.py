977f4544c0c3092ad77bc4dff166e43b80461593
Shelton Gayles <sgbythelake@gmail.com>
2019-03-31 19:40:42 -0700

    added item list route

--- code/app.py
+++ code/app.py
@@ -1,4 +1,4 @@
-from flask import Flask 
+from flask import Flask , request 
 from flask_restful import Resource, Api 
 
 app = Flask(__name__)
@@ -16,12 +16,19 @@ class Item(Resource):
 
     
     def post(self, name):
-        item = {'name': name, 'price': 12.00}
+        data = request.get_json()
+        item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201
 
+
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
+
+
 api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
 
-
-app.run(port=5000)
+app.run(port=5000, debug=True)
 
