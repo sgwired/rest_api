4bc96692c257cacc7630ffddd0c0ee99205ace3d
Shelton Gayles <sgbythelake@gmail.com>
2019-03-31 19:11:17 -0700

    switched student to item

--- code/app.py
+++ code/app.py
@@ -4,11 +4,14 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-class Student(Resource):
-    def get(self, name):
-        return {'student': name}
+items = []
 
-api.add_resource(Student, '/student/<string:name>')
+
+class Item(Resource):
+    def get(self, name):
+        return {'item': name}
+
+api.add_resource(Item, '/item/<string:name>')
 
 
 app.run(port=5000)
