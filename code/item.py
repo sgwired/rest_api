26c55c2068022bf285f5c0b277850daeda652eb0
Shelton Gayles <sgbythelake@gmail.com>
2019-04-06 12:44:20 -0700

    move the insert to cls method

--- code/item.py
+++ code/item.py
@@ -39,6 +39,17 @@ class Item(Resource):
         data = Item.parser.parse_args()
         
         item = {'name': name, 'price': data['price']}
+
+        try: 
+            self.insert(item)
+        except:
+            return {"message": "An error occured inserting the item"}, 500
+        
+        return item, 201
+
+
+    @classmethod
+    def insert(cls, item):
         connection = sqlite3.connect('data.db')
         cursor = connection.cursor()
         query = "INSERT INTO items VALUES(?, ?)"
@@ -46,7 +57,6 @@ class Item(Resource):
         connection.commit()
         connection.close()
 
-        return item, 201
 
     def delete(self, name):
         connection = sqlite3.connect('data.db')
