a3e9ad5d95fb36889a2607d849390bf02950434f
Shelton Gayles <sgbythelake@gmail.com>
2019-04-06 17:06:28 -0700

    implemented the user model

--- code_sa/models/user.py
+++ code_sa/models/user.py
@@ -9,42 +9,19 @@ class UserModel(db.Model):
     password = db.Column(db.String(80))
     
 
-    def __init__(self, _id, username, password):
-        self.id = _id
+    def __init__(self, username, password):
         self.username = username
         self.password = password
 
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
     @classmethod # not using self so class method
     def find_by_username(cls, username):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM users WHERE username=?"
-        result = cursor.execute(query, (username,)) #tuple need trailing comma
-        row = result.fetchone()
-        if row:
-            user = cls(*row) # same as cls(row[0], row[1], row[2])
-        else:
-            user = None
-        
-
-        connection.close()
-        return user
-
+        return cls.query.filter_by(username=username).first()
 
     @classmethod
     def find_by_id(cls, _id):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM users WHERE id=?"
-        result = cursor.execute(query, (_id,))
-        row = result.fetchone()
-        if row:
-            user = cls(*row)
-        else:
-            user = None
+        return cls.query.filter_by(id=_id).first()
         
-
-        connection.close()
-        return user
